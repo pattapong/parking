67d2e8d68097e424dfc60afcec02422a5c50f253
pattapong <pattapong@gmail.com>
2015-06-10 20:41:37 -0400

    Add

---
+++ engine/src/main/java/parking/engine/ParkingFeeIdentifier.java
@@ -0,0 +1,37 @@
+package parking.engine;
+
+import org.json.JSONArray;
+
+import parking.om.ParkingFee;
+
+public class ParkingFeeIdentifier {
+
+	
+	/**
+	 * Get JSON array of parkingFee ID
+	 * @param parkingId parking id
+	 * @return The JSON array of parking fee id
+	 */
+	public JSONArray getParkingFeeJSONArray(final int parkingId){
+		return null;
+	}
+
+	/**
+	 * Get ParkingFee object from parkingFeeId
+	 * @param parkingFeeId parking fee ID
+	 * @return ParkingFee object
+	 */
+	public ParkingFee getParkingFeeFromId(final int parkingFeeId){
+		return null;
+	}
+	
+	/**
+	 * To compare the current time with start/end time of the parkingFee Object, to determine
+	 * It is free to park or not
+	 * @param parkingFee ParkingFee object
+	 * @return true if the parking is free to park, otherwise false
+	 */
+	public boolean isFree(ParkingFee parkingFee){
+		return false;
+	}
+}
---
+++ engine/src/main/java/parking/engine/ParkingLocationIdentifier.java
@@ -0,0 +1,16 @@
+package parking.engine;
+
+import parking.om.ParkingCoordinate;
+
+public class ParkingLocationIdentifier {
+
+	/**
+	 * Get parking id by using the current coordinate to compare with start/end coordinate of the parkingLocation object
+	 * @param parkingCoordinate
+	 * @return
+	 */
+	public int getParkingId(final ParkingCoordinate parkingCoordinate) {
+		return 0;
+	}
+
+}
